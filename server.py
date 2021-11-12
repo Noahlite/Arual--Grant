eccc39807c35578fad946f5ab8b636e2ad584977
Noahlite <93860495+Noahlite@users.noreply.github.com>
2021-11-12 21:42:10 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
 
-from flask import Flask, render_template,request,redirect
+from flask import Flask, render_template,request,redirect,url_for
 
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,TextAreaField,IntegerField, SelectField
+from wtforms import StringField,SubmitField,IntegerField, SelectField
 from wtforms.validators import DataRequired, Email, Length
 from flask_bootstrap import Bootstrap
-from twilio.rest import Client
-
+from pymongo import MongoClient
 
 
 
@@ -19,14 +18,15 @@ class Form(FlaskForm):
     fname = StringField(label='First Name', validators=[DataRequired()])
     lname = StringField(label='Last Name', validators=[DataRequired()])
     email = StringField(label='Email', validators=[DataRequired(), Email()])
-    phone = IntegerField(label='Phone_number', validators=[DataRequired(),Length(min=10)])
+    phone = IntegerField(label='Phone_number', validators=[DataRequired()])
     select = SelectField(label='Help', choices=('Children 👶 ', 'Animals 🐶'))
-    text = TextAreaField(label='How much do you wish to donate? ')
+    text = IntegerField(label='How much do you wish to donate? ',validators=[DataRequired()])
     submit = SubmitField(label='Submit')
 
 
-Twillio_auth = "5d0e5effc0d6ac6525b5179b9c3edd41"
-Twillio_ssid = "AC422603a9160fa586da1cbd1e517c03fd"
+client =MongoClient("mongodb+srv://test:test@cluster0.gvn73.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client.get_database('charity_db')
+records = db.charity_records
 @app.route('/contact')
 def contact():
     return redirect('http://eepurl.com/hMUHeb')
@@ -43,9 +43,9 @@ def about():
 @app.route('/donate', methods=['GET', 'POST'])
 def donate():
     login_form = Form()
-    client= Client(Twillio_ssid,Twillio_auth)
+    # client= Client(Twillio_ssid,Twillio_auth)
 
-    if request.method == 'POST':
+    if login_form.validate_on_submit():
         fname = login_form.fname.data
         lname = login_form.lname.data
         Email = login_form.email.data
@@ -53,22 +53,16 @@ def donate():
         phone = login_form.phone.data
         select = login_form.select.data
 
-        message = f'{fname}-{lname} wants to pay\n' \
-                  f'${amount}\n'\
-                  f' for {select}\n'\
-                  f'{Email}\n' \
-                  f'{phone}'
-
-        client.messages.create(from_="+12566175621",
-                               to="+18153176218",
-                               body=message)
-
-
-
-
-
+        charity_giver = {'fname':fname,
+                         'lname':lname,
+                         'email':Email,
+                         'phone':phone,
+                         'select':amount,
+                         'amount':select}
+        records.insert_one(charity_giver)
+        return redirect(url_for('home'))
     return render_template('contact.html', form=login_form)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
